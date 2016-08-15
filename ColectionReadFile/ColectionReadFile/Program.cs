9a36855df325b6786e5893879ac747d107504b12
Sviatoslav Komarnitskiy <sv.wolf75@gmail.com>
2016-08-15 14:55:14 +0300

    Collection works with txt file

---
+++ ColectionReadFile/ColectionReadFile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColectionReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StoreCollection collection = new StoreCollection(@"C:\Project\5645.txt");
+
+            collection.Add(-32145);
+            foreach (int i in collection)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
