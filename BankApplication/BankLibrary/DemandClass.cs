58821b613b765a8b9783e19787b08b4e80efd5c0
Sviatoslav Komarnitskiy <sv.wolf75@gmail.com>
2016-11-03 16:00:22 +0200

    Add solution BankApplication

---
+++ BankApplication/BankLibrary/DemandClass.cs
@@ -0,0 +1,16 @@
+﻿using BankLibrary;
+
+namespace BankApplication
+{
+    public class DemandAccount : Account
+    {
+        public DemandAccount(decimal sum, int percentage) : base(sum, percentage)
+        {
+        }
+
+        protected internal override void Open()
+        {
+            base.OnOpened(new AccountEventArgs("Открыт новый счет до востребования! Id счета: " + this._id, this._sum));
+        }
+    }
+}
